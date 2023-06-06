3ca9e3a2554467be3d4cd7134b913600324800f5
IamBansal <abansal11ae@gmail.com>
2023-06-06 15:13:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/rehlattask1/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.rehlattask1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.rehlattask1.databinding.ActivityMainBinding
+import com.google.android.material.appbar.AppBarLayout
+import kotlin.math.abs
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setCustomToolbarAfterCollapsing()
+    }
+
+    private fun setCustomToolbarAfterCollapsing() {
+        binding.appBar.addOnOffsetChangedListener(object : AppBarLayout.OnOffsetChangedListener {
+
+            var isCustomToolbarVisible = false
+
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                val maxScroll = appBarLayout.totalScrollRange
+                val percentage = abs(verticalOffset).toFloat() / maxScroll.toFloat()
+
+                // Show/hide custom toolbar based on the collapse percentage
+                if (percentage >= 0.8f && !isCustomToolbarVisible) {
+                    binding.customToolbar.visibility = View.VISIBLE
+                    binding.collapsingToolbar.visibility = View.INVISIBLE
+                    isCustomToolbarVisible = true
+                } else if (percentage < 0.8f && isCustomToolbarVisible) {
+                    binding.customToolbar.visibility = View.GONE
+                    binding. collapsingToolbar.visibility = View.VISIBLE
+                    isCustomToolbarVisible = false
+                }
+            }
+        })
+    }
+}
